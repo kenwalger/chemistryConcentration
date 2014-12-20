9c5a93b52bf49ce6ac4fdfc22a790152e990748e
Ken W. Alger <kenalger@comcast.net>
2014-12-20 00:15:44 -0800

    First half of cation images

--- anion.php
+++ anion.php
@@ -33,7 +33,6 @@ include ('inc/header.php');
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" /></script>
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js" /></script>
-         <script src="js/script.min.js" /></script>
         <script src="js/jquery.flip.min.js" /></script>
         <script src="js/jquery.quizymemorygame.js" /></script>
 
