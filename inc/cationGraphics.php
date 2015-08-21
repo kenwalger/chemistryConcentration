e5a6e12d2335e795ba33c9a42eb7be0563604194
Ken W. Alger <kenalger@comcast.net>
2015-08-20 23:08:09 -0700

    update

---
+++ inc/cationGraphics.php
@@ -0,0 +1,127 @@
+                <div id="my-memorygame" class="quizy-memorygame large-12 push-2 columns cations">
+                    <ul>
+                      <li class="match1">
+                        <img src="img/cations/aluminum.gif" alt="Aluminum">
+                      </li>
+                      <li class="match1">
+                        <img src="img/cations/aluminumFormula.gif" alt="Al3+">
+                      </li>
+                      <li class="match2">
+                        <img src="img/cations/ammonium.gif" alt="Ammonium">
+                      </li>
+                      <li class="match2">
+                        <img src="img/cations/ammoniumFormula.gif" alt="NH4+">
+                      </li>
+                      <li class="match3">
+                        <img src="img/cations/barium.gif" alt="Barium">
+                      </li>
+                      <li class="match3">
+                        <img src="img/cations/bariumFormula.gif" alt="Ba2+">
+                      </li>
+                      <li class="match4">
+                        <img src="img/cations/calcium.gif" alt="Calcium">
+                      </li>
+                      <li class="match4">
+                        <img src="img/cations/calciumFormula.gif" alt="Ca2+">
+                      </li>
+                      <li class="match5">
+                        <img src="img/cations/chromium.gif" alt="Chromium">
+                      </li>
+                      <li class="match5">
+                        <img src="img/cations/chromium3Formula.gif" alt="Cr3+">
+                      </li>
+                      <li class="match6">
+                        <img src="img/cations/copper1.gif" alt="Copper(I)">
+                      </li>
+                      <li class="match6">
+                        <img src="img/cations/copper1Formula.gif" alt="Cu+">
+                      </li>
+                      <li class="match7">
+                        <img src="img/cations/copper2.gif" alt="Copper(II)">
+                      </li>
+                      <li class="match7">
+                        <img src="img/cations/copper2Formula.gif" alt="Cu2+">
+                      </li>
+                      <li class="match8">
+                        <img src="img/cations/hydrogen.gif" alt="Hydrogen">
+                      </li>
+                      <li class="match8">
+                        <img src="img/cations/hydrogenFormula.gif" alt="H+">
+                      </li>
+                      <li class="match9">
+                        <img src="img/cations/iron2.gif" alt="Iron(II)">
+                      </li>
+                      <li class="match9">
+                        <img src="img/cations/iron2Formula.gif" alt="Fe2+">
+                      </li>
+                      <li class="match10">
+                        <img src="img/cations/iron3.gif" alt="Iron(III)">
+                      </li>
+                      <li class="match10">
+                        <img src="img/cations/iron3Formula.gif" alt="Fe3+">
+                      </li>
+                      <li class="match11">
+                        <img src="img/cations/lead2.gif" alt="Lead(II)">
+                      </li>
+                      <li class="match11">
+                        <img src="img/cations/lead2Formula.gif" alt="Pb2+">
+                      </li>
+                      <li class="match12">
+                        <img src="img/cations/lead4.gif" alt="Lead(IV)">
+                      </li>
+                      <li class="match12">
+                        <img src="img/cations/lead4Formula.gif" alt="Pb4+">
+                      </li>
+                      <li class="match13">
+                        <img src="img/cations/lithium.gif" alt="Lithium">
+                      </li>
+                      <li class="match13">
+                        <img src="img/cations/lithiumFormula.gif" alt="Li+">
+                      </li>
+                      <li class="match14">
+                        <img src="img/cations/magnesium.gif" alt="Magnesium">
+                      </li>
+                      <li class="match14">
+                        <img src="img/cations/magnesiumFormula.gif" alt="Mg2+">
+                      </li>
+                      <li class="match15">
+                        <img src="img/cations/mercury2.gif" alt="Mercury(II)">
+                      </li>
+                      <li class="match15">
+                        <img src="img/cations/mercury2Formula.gif" alt="Hg2+">
+                      </li>
+                      <li class="match16">
+                        <img src="img/cations/nickel2.gif" alt="Nickel(II)">
+                      </li>
+                      <li class="match16">
+                        <img src="img/cations/nickel2Formula.gif" alt="Ni2+">
+                      </li>
+                      <li class="match17">
+                        <img src="img/cations/potassium.gif" alt="Potassium">
+                      </li>
+                      <li class="match17">
+                        <img src="img/cations/potassiumFormula.gif" alt="K+">
+                      </li>
+                      <li class="match18">
+                        <img src="img/cations/silver.gif" alt="Silver">
+                      </li>
+                      <li class="match18">
+                        <img src="img/cations/silverFormula.gif" alt="Ag+">
+                      </li>
+                       <li class="match19">
+                        <img src="img/cations/sodium.gif" alt="Sodium">
+                      </li>
+                      <li class="match19">
+                        <img src="img/cations/sodiumFormula.gif" alt="Na+">
+                      </li>
+                      <li class="match20">
+                        <img src="img/cations/zinc.gif" alt="Zinc">
+                      </li>
+                      <li class="match20">
+                        <img src="img/cations/zincFormula.gif" alt="Zn2+">
+                      </li>                     
+                    </ul>
+                </div>
+                <div class="reset-button">
+                    <a class="button tiny primary radius" id="restart" href="">Reset game</a>
+                </div>
